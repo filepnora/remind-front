4882e1fb397efb22add723388cb516ebd79847c4
Nora Filep <filepnora@gmail.com>
2019-09-22 21:52:23 +0200

    homepage init

---
+++ src/components/Home/Home.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+import './Home.css';
+
+class Home extends Component {
+    render() {
+        return (
+            <div>
+                <p>this is the home page</p>
+            </div>
+        )
+    }
+}
+
+export default Home;

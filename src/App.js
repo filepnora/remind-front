2fa605616f650156bcc65f92eb8f2ac99911ebaf
Nora Filep <filepnora@gmail.com>
2019-09-24 23:08:04 +0200

    routing for add-page components

--- src/App.js
+++ src/App.js
@@ -5,25 +5,29 @@ import Footer from './components/Footer/Footer';
 import Home from './components/Home/Home';
 import LoginPage from "./components/Login/LoginPage";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import NewBookMediaPage from "./components/AddNewPage/NewBookMediaPage";
+import NewNotePage from "./components/AddNewPage/NewNotePage";
 
 class App extends Component {
 
-  render() {
-    return (
-        <Router>
-          <div className="App">
-            <Header/>
-          </div>
-          <div className="body">
-            <Switch>
-              <Route exact path={'/'} component={Home}/>
-              <Route exact path={'/login'} component={LoginPage}/>
-            </Switch>
-          </div>
-          <Footer/>
-        </Router>
-    );
-  }
+    render() {
+        return (
+            <Router>
+                <div className="App">
+                    <Header/>
+                </div>
+                <div className="body">
+                    <Switch>
+                        <Route exact path={'/'} component={Home}/>
+                        <Route exact path={'/login'} component={LoginPage}/>
+                        <Route path="/(new-book|new-media)/" component={NewBookMediaPage}/>
+                        <Route path="/new-note" component={NewNotePage}/>
+                    </Switch>
+                </div>
+                <Footer/>
+            </Router>
+        );
+    }
 }
 
 export default App;

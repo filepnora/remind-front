5280227ea5323a5a59d65d6f73bb38ad38c9a82a
Nora Filep <filepnora@gmail.com>
2019-09-24 10:31:35 +0200

    card styling

--- src/components/Card/HomePageCardGroup.js
+++ src/components/Card/HomePageCardGroup.js
@@ -4,23 +4,33 @@ import CardDeck from "react-bootstrap/CardDeck";
 import bookImg from "../../resources/add-book.png";
 import mediaImg from "../../resources/video-document-add-button.png";
 import notePadImg from "../../resources/notepad.png";
+import './Card.css';
 
 class HomePageCardGroup extends Component {
     render() {
         return (
-            <CardDeck>
+            <CardDeck className="deck">
                 <NewItemCard
+                    color="rgb(254, 255, 172)"
+                    buttonColor="yellow"
                     image={bookImg}
-                    title="new Book"
-                    content="you can add a new BOOK you want to read, read, will read here.."/>
+                    title="Book"
+                    content="you want to read, read, will read here.."
+                    link="/new-book"/>
                 <NewItemCard
+                    color="rgb(255, 172, 213)"
+                    buttonColor="#ff28c5"
                     image={mediaImg}
-                    title="new Movie / Series"
-                    content="you can add a new MOVIE / SERIES you want to watch, saw, will watch.."/>
+                    title="Movie / Series"
+                    content="you want to watch, saw, will watch.."
+                    link="/new-media"/>
                 <NewItemCard
+                    color="rgb(172, 255, 187)"
+                    buttonColor="#009767"
                     image={notePadImg}
-                    title="new Note"
-                    content="you can add a new NOTE you want to ... remember"/>
+                    title="Note"
+                    content="you want to ... remember"
+                    link="new-note/"/>
             </CardDeck>
         )
     }
--- src/components/Card/NewItemCard.js
+++ src/components/Card/NewItemCard.js
@@ -1,25 +1,29 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Card} from "react-bootstrap";
 import './Card.css';
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
 
 const NewItemCard = (props) => {
     return (
-        <Card className="homePageCard">
-                <Row>
-                    <Col>
-                        <Card.Img variant="top" className="homePageCardImg" src={props.image}/>
-                    </Col>
-                    <Col>
-                        <Card.Body>
-                            <Card.Title className="homePageCardTitle">{props.title}</Card.Title>
-                            <Card.Text>
-                                {props.content}
-                            </Card.Text>
-                        </Card.Body>
-                    </Col>
-                </Row>
+        <Card className="homePageCard" style={{backgroundColor: props.color}}>
+            <Row>
+                <Col className="imgCol">
+                    <Card.Img variant="top" className="homePageCardImg" src={props.image}/>
+                </Col>
+                <Col className="textCol">
+                    <Card.Body>
+                        <Card.Title className="homePageCardTitle">
+                            <Button className="addnewbutton" href={props.link} style={{backgroundColor: "#282c34"}}>Add {props.title}</Button>
+                        </Card.Title>
+                        <br/>
+                        <Card.Text>
+                            {props.content}
+                        </Card.Text>
+                    </Card.Body>
+                </Col>
+            </Row>
         </Card>
     )
 };

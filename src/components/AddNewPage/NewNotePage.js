4f7ce18484a1e15a8ad9a26ef6521440e8d2a90e
Nora Filep <filepnora@gmail.com>
2019-09-24 23:08:31 +0200

    add new note page init

---
+++ src/components/AddNewPage/NewNotePage.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+import Form from "react-bootstrap/Form";
+import {Card} from "react-bootstrap";
+import './AddNew.css';
+import Button from "react-bootstrap/Button";
+
+class NewNotePage extends Component {
+    render() {
+        return (
+            <Form className="pageTitle center newNoteCard">
+                <h1>Add a new NOTE</h1>
+                <hr/>
+                <Card style={{backgroundColor: "rgb(254, 255, 172)"}}>
+                    <Form.Group className="newNoteCardInputs">
+                        <Form.Label><h4>Title:</h4></Form.Label>
+                        <Form.Control type="text" placeholder="" className="center"/>
+                        <p/>
+                        <Form.Label>Content:</Form.Label>
+                        <Form.Control as="textarea" rows="6"/>
+                    </Form.Group>
+                    <Button variant="primary" type="submit" className="submitButton center">
+                        Submit
+                    </Button>
+                </Card>
+            </Form>
+        )
+    }
+}
+
+export default NewNotePage;

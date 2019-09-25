c40ec11d536095f645a1d25721a4374ab916d801
Nora Filep <filepnora@gmail.com>
2019-09-25 20:41:58 +0200

    new components included in routing

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import LoginPage from "./components/Login/LoginPage";
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import NewBookMediaPage from "./components/AddNewPage/NewBookMediaPage";
 import NewNotePage from "./components/AddNewPage/NewNotePage";
+import ListPage from "./components/ListPage/ListPage";
+import NoteListPage from "./components/ListPage/NoteListPage";
 
 class App extends Component {
 
@@ -20,8 +22,13 @@ class App extends Component {
                     <Switch>
                         <Route exact path={'/'} component={Home}/>
                         <Route exact path={'/login'} component={LoginPage}/>
-                        <Route path="/(new-book|new-media)/" component={NewBookMediaPage}/>
-                        <Route path="/new-note" component={NewNotePage}/>
+                        <Route exact path={'/new-book'} component={NewBookMediaPage}/>
+                        <Route exact path={'/new-media'} component={NewBookMediaPage}/>
+                        {/*<Route path={"/new-book"} render={(props) => <NewBookMediaPage {...props} mediaType={book}/>}/>
+                        <Route path={"/new-media"} render={(props) => <NewBookMediaPage {...props} mediaType={media}/>}/>*/}
+                        <Route exact path={"/new-note"} component={NewNotePage}/>
+                        <Route exact path={'/books'} component={ListPage}/>
+                        <Route exact path={'/notes'} component={NoteListPage}/>
                     </Switch>
                 </div>
                 <Footer/>

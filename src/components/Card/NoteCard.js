37d1899f4661f2e474a8b294fbd0d6366f488ef8
Nora Filep <filepnora@gmail.com>
2019-09-28 22:02:28 +0200

    note list page fetches data from backend

--- src/components/Card/NoteCard.js
+++ src/components/Card/NoteCard.js
@@ -1,37 +1,30 @@
-import React, {Component} from "react";
+import React from "react";
 import './Card.css';
 import {Button, Card, Image} from "react-bootstrap";
 import editIcon from '../../resources/edit.png'
 import binIcon from '../../resources/garbage.png'
 import Row from "react-bootstrap/Row";
 
-class NoteCard extends Component {
-    render() {
-        return (
-            <Card className="listPageNoteCard">
-                <Card.Body>
-                    <Card.Title className="center">
-                        <Row>
-                            <h3>This is a note</h3>
-                            <Image className="smallButton" src={binIcon} style={{right: "1em"}}/>
-                            <Image className="smallButton" src={editIcon} style={{right: "4em"}}/>
-                        </Row>
-                    </Card.Title>
-                    <hr/>
-                    <Card.Text>
-                        <p>
-                            some content
-                            some content
-                            some content
-                            some content
-                            some content
-                            some content
-                        </p>
-                    </Card.Text>
-                </Card.Body>
-            </Card>
-        )
-    }
+const NoteCard = (props) => {
+    return (
+        <Card className="listPageNoteCard">
+            <Card.Body>
+                <Card.Title className="center">
+                    <Row>
+                        <h3>{props.title}</h3>
+                        <Image className="smallButton" src={binIcon} style={{right: "1em"}}/>
+                        <Image className="smallButton" src={editIcon} style={{right: "4em"}}/>
+                    </Row>
+                </Card.Title>
+                <hr/>
+                <Card.Text>
+                    <p>
+                        {props.content}
+                    </p>
+                </Card.Text>
+            </Card.Body>
+        </Card>
+    )
 }
 
 export default NoteCard;
--- src/components/ListPage/NoteListPage.js
+++ src/components/ListPage/NoteListPage.js
@@ -4,14 +4,44 @@ import '../Card/Card.css'
 import CardColumns from "react-bootstrap/CardColumns";
 
 class NoteListPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            noteData: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:8080/notes`)
+            .then(response => response.json())
+            .then((result) => {
+                this.setState({
+                    isLoaded: true,
+                    noteData: result
+                })
+            })
+            .catch(err => {
+                throw new Error(err)
+            })
+    }
+
+    getNoteData() {
+        let noteInfo = [];
+        for (let i = 0; i < this.state.noteData.length; i++) {
+            let note = this.state.noteData[i];
+            noteInfo.push(
+                <NoteCard title={note.title} content={note.content}/>
+            )
+        }
+        return noteInfo;
+    }
+
+
     render() {
         return (
             <CardColumns style={{marginRight: "6em"}}>
-                <NoteCard/>
-                <NoteCard/>
-                <NoteCard/>
-                <NoteCard/>
-                <NoteCard/>
+                {this.getNoteData()}
             </CardColumns>
         )
     }

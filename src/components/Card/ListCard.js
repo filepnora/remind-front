72c8b8d46e40805079eb92de0d4a523fc2f18e33
Nora Filep <filepnora@gmail.com>
2019-09-30 17:54:45 +0200

    book list page data fetched from backend

--- src/components/Card/ListCard.js
+++ src/components/Card/ListCard.js
@@ -2,24 +2,26 @@ import React, {Component} from "react";
 import Card from "react-bootstrap/Card";
 import "./Card.css"
 
-class ListCard extends Component {
-    render() {
-        return (
-            <Card className="listPageCard">
-                <Card.Img variant="top" src="" />
-                <Card.Body>
-                    <Card.Title>Card title</Card.Title>
-                    <Card.Text>
-                        This is a wider card with supporting text below as a natural lead-in to
-                        additional content. This content is a little bit longer.
-                    </Card.Text>
-                </Card.Body>
-                <Card.Footer>
-                    <small className="text-muted">Last updated 3 mins ago</small>
-                </Card.Footer>
-            </Card>
-        )
-    }
-}
+const ListCard = (props) => {
+    return (
+        <Card className="listPageCard">
+            <Card.Img variant="top" src=""/>
+            <Card.Body>
+                <Card.Title className="center"><h2>{props.title}</h2></Card.Title>
+                <hr/>
+                <Card.Text>
+                    <p><b>Author: </b>{props.author}</p>
+                    <hr/>
+                    <p><b>Publication date: </b>{props.publicationDate}</p>
+                    <hr/>
+                    <p>Status: {props.status}</p>
+                </Card.Text>
+            </Card.Body>
+            <Card.Footer>
+                <small className="text-muted">Added on {props.added}</small>
+            </Card.Footer>
+        </Card>
+    )
+};
 
 export default ListCard;
--- src/components/ListPage/ListPage.js
+++ src/components/ListPage/ListPage.js
@@ -2,14 +2,46 @@ import React, {Component} from "react";
 import ListCard from "../Card/ListCard";
 import CardDeck from "react-bootstrap/CardDeck";
 import "../Card/Card.css"
+import NoteCard from "../Card/NoteCard";
 
 class ListPage extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            isLoaded: false,
+            mediaData: []
+        }
+    }
+
+    componentDidMount() {
+        fetch(`http://localhost:8080/books`)
+            .then(response => response.json())
+            .then((result) => {
+                this.setState({
+                    isLoaded: true,
+                    mediaData: result
+                })
+            })
+            .catch(err => {
+                throw new Error(err)
+            })
+    }
+
+    getMediaData() {
+        let noteInfo = [];
+        for (let i = 0; i < this.state.mediaData.length; i++) {
+            let media = this.state.mediaData[i];
+            noteInfo.push(
+                <ListCard title={media.title} author={media.author} publicationDate={media.publicationDate} status={media.status} added={media.added}/>
+            )
+        }
+        return noteInfo;
+    }
+
     render() {
         return (
             <CardDeck className="deck">
-                <ListCard/>
-                <ListCard/>
-                <ListCard/>
+                {this.getMediaData()}
             </CardDeck>
         )
     }
